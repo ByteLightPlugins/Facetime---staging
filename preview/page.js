52a8ab9b4c2481b7579583369523d0540235cb80
ByteLight Plugin Repositories <plugins@bytelight.com>
2014-11-04 00:02:44 +0000

    Released on: 2014-11-04 00:02:43 +0000

---
+++ preview/page.js
@@ -0,0 +1,8 @@
+if(BL.isTablet()){
+  $('.overlay').addClass('tablet')
+}
+else{
+  $('.overlay').addClass('phone')
+  $('.headline').addClass('phone-headline')
+  $('.preview').addClass('phone-preview')
+}
